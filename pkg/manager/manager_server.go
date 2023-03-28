adb4ef49f3bd5b438f7f738fb2f57d140ca01a1f
Wenhao Jiang <whjiang1997@gmail.com>
2023-03-28 20:11:48 +0800

    Add listpod rpc function

--- pkg/manager/manager_server.go
+++ pkg/manager/manager_server.go
@@ -19,3 +19,19 @@ func (s *DaemonServer) GetKubernetesVersion(ctx context.Context, in *pb.Kubernet
 	}
 	return &pb.KubernetesVersionReply{Version: kubernetesVersion}, nil
 }
+
+func (s *DaemonServer) ListPod(ctx context.Context, in *pb.ListPodRequest) (*pb.ListPodResponse, error) {
+	pods, err := s.provider.ListPod(ctx, in.GetNamespace())
+	if err != nil {
+		logrus.Error(err)
+	}
+	podList := []*pb.Pod{}
+	for _, pod := range pods {
+		podList = append(podList, &pb.Pod{
+			Name:      pod.Name,
+			Namespace: pod.Namespace,
+		})
+	}
+
+	return &pb.ListPodResponse{PodList: podList}, nil
+}
